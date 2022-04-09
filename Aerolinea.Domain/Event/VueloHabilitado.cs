13179a0f567ea3ca77298a62fd14f883f91ea9fa
ronal.inturias <ronal.95.03@gmail.com>
2022-04-09 08:26:39 -0400

    se realizo el microservicio de vuelos el modelo de dominio

---
+++ Aerolinea.Domain/Event/VueloHabilitado.cs
@@ -0,0 +1,25 @@
+﻿using Sharedkernel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerolinea.Vuelos.Domain.Event
+{
+    public class VueloHabilitado : DomainEvent
+    {
+
+
+
+
+        public VueloHabilitado(Vuelo vuelo) : base(DateTime.Now)
+        {
+
+            vueloHabilitado = vuelo;
+
+        }
+        public Vuelo vueloHabilitado { get; private set; }
+    
+    }
+}
